df3ed9e96df8b384ffcc3e587140f4e3e1069788
Paulo Gonçalves <paulomontealtomac@outlook.com>
2025-05-13 12:12:16 -0300

    Portifolio BackEnd

---
+++ Portifólio/script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        window.scrollTo({
+            top: targetElement.offsetTop - 50,
+            behavior: 'smooth'
+        });
+    });
+});
