34b2eb6be7b625f59ce838e711b70f1daf94c20e
Paulo Gonçalves <paulomontealtomac@outlook.com>
2025-08-23 22:43:02 -0300

    atulaziando versão desktop

--- Portifólio/script.js
+++ Portifólio/script.js
@@ -1,11 +1,7 @@
 document.querySelectorAll('nav ul li a').forEach(link => {
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-        window.scrollTo({
-            top: targetElement.offsetTop - 50,
-            behavior: 'smooth'
-        });
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const target = document.querySelector(link.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth', block: 'start' });
     });
 });
